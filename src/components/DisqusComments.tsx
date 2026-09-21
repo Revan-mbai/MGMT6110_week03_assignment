9b7ab735a40ab5a65b88f7145a81b48bbe589c34
Revan-mbai <revan.koh.2026@smu.edu.sg>
2026-09-21 20:04:33 +0800

    Update DisqusComments.tsx

--- src/components/DisqusComments.tsx
+++ src/components/DisqusComments.tsx
@@ -12,7 +12,7 @@ declare global {
   }
 }
 
-const DISQUS_SHORTNAME = 'sgbusTracker';
+const DISQUS_SHORTNAME = 'sgbustracker';
 const PAGE_URL = 'https://mgmt-6110-week03-assignment.vercel.app/';
 const PAGE_IDENTIFIER = 'home';
 const SCRIPT_ID = 'disqus-embed-script';
